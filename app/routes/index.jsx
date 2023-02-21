824aa5a1585fe1554b3079b500b508819eab85c1
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-02-21 13:02:54 +0300

    Sidebar düzenlenmesi

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,64 +1,15 @@
 import { NavLink } from "@remix-run/react";
 import Feed from "../components/Feed"
 import Widget from "../components/Widget"
+import SideBar from "../components/SideBar";
 
 
 export default function Index() {
-  return (<div className="flex justify-center grid-cols-3">
+  return (
+  <div className="flex min-h-screen max-w-7xl mx-auto  justify-center grid-cols-3">
     <div className=" grid grid-cols-3 divide-x ">
       <div className="sticky top-0 z-10   h-16    ">
-      
-      
-      <nav className="fixed bottom-0 flex h-16 w-full flex-auto flex-col border-t border-gray-200 bg-white sm:static sm:bottom-auto sm:h-auto sm:border-none gap-3 items-center"> 
-        <NavLink to="/" className="hidden h-16 sm:block">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
-        </NavLink>
-      
-    <NavLink to="/" className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <div>
-    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M22 11.414v12.586h-20v-12.586l-1.293 1.293-.707-.707 12-12 12 12-.707.707-1.293-1.293zm-6 11.586h5v-12.586l-9-9-9 9v12.586h5v-9h8v9zm-1-7.889h-6v7.778h6v-7.778z"/></svg></div>
-    <div><span>&nbsp;Home</span></div>
-    </NavLink>
-    
-    
-      <NavLink to="/" className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex ">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22.548 9l.452-2h-5.364l1.364-6h-2l-1.364 6h-5l1.364-6h-2l-1.364 6h-6.184l-.452 2h6.182l-1.364 6h-5.36l-.458 2h5.364l-1.364 6h2l1.364-6h5l-1.364 6h2l1.364-6h6.185l.451-2h-6.182l1.364-6h5.366zm-8.73 6h-5l1.364-6h5l-1.364 6z"/></svg><div><span>&nbsp;Explore</span></div>
-      </NavLink>
-
-    
-      <NavLink to="/" className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M1 20v-2.946c1.993-.656 2.575-2.158 3.668-6.077.897-3.218 1.891-6.784 4.873-8.023-.027-.147-.041-.299-.041-.454 0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5c0 .156-.014.309-.042.458 2.987 1.244 3.984 4.813 4.884 8.033 1.103 3.95 1.697 5.423 3.658 6.062v2.947h-7c0 2.208-1.792 4-4 4s-4-1.792-4-4h-7zm14 0h-6c0 1.656 1.344 3 3 3s3-1.344 3-3zm-13-1h20v-1.241c-2.062-.918-2.82-3.633-3.621-6.498-1.066-3.814-2.167-7.758-6.379-7.761-4.21 0-5.308 3.937-6.369 7.745-.8 2.872-1.559 5.593-3.631 6.514v1.241zm11.492-16.345l.008-.155c0-.828-.672-1.5-1.5-1.5s-1.5.672-1.5 1.5l.008.152c.455-.099.949-.152 1.487-.152.542 0 1.039.054 1.497.155z"/></svg>
-      <div><span>&nbsp;Notification</span></div>
-        
-      </NavLink>
-  
-    
-      <NavLink to="/" className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z"/></svg><div><span>&nbsp;Messages</span></div>
-      </NavLink>
-    
-      <NavLink to="/" className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M5 0v24l7-6 7 6v-24h-14zm1 1h12v20.827l-6-5.144-6 5.144v-20.827z"/></svg>
-      <div><span>&nbsp;Bookmarks</span></div>
-        
-      </NavLink>
-    
-      <NavLink to="/" className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <svg width="24" height="24"  clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m21 4c0-.478-.379-1-1-1h-16c-.62 0-1 .519-1 1v16c0 .621.52 1 1 1h16c.478 0 1-.379 1-1zm-16.5.5h15v15h-15zm12.5 10.75c0-.414-.336-.75-.75-.75h-8.5c-.414 0-.75.336-.75.75s.336.75.75.75h8.5c.414 0 .75-.336.75-.75zm0-3.248c0-.414-.336-.75-.75-.75h-8.5c-.414 0-.75.336-.75.75s.336.75.75.75h8.5c.414 0 .75-.336.75-.75zm0-3.252c0-.414-.336-.75-.75-.75h-8.5c-.414 0-.75.336-.75.75s.336.75.75.75h8.5c.414 0 .75-.336.75-.75z" fillRule="nonzero"/></svg>
-      <div><span>&nbsp;Lists</span></div>
-      </NavLink>
-      
-    
-      <NavLink to="/"className="flex  hover:bg-gray-500/10 rounded-full border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm8.127 19.41c-.282-.401-.772-.654-1.624-.85-3.848-.906-4.097-1.501-4.352-2.059-.259-.565-.19-1.23.205-1.977 1.726-3.257 2.09-6.024 1.027-7.79-.674-1.119-1.875-1.734-3.383-1.734-1.521 0-2.732.626-3.409 1.763-1.066 1.789-.693 4.544 1.049 7.757.402.742.476 1.406.22 1.974-.265.586-.611 1.19-4.365 2.066-.852.196-1.342.449-1.623.848 2.012 2.207 4.91 3.592 8.128 3.592s6.115-1.385 8.127-3.59zm.65-.782c1.395-1.844 2.223-4.14 2.223-6.628 0-6.071-4.929-11-11-11s-11 4.929-11 11c0 2.487.827 4.783 2.222 6.626.409-.452 1.049-.81 2.049-1.041 2.025-.462 3.376-.836 3.678-1.502.122-.272.061-.628-.188-1.087-1.917-3.535-2.282-6.641-1.03-8.745.853-1.431 2.408-2.251 4.269-2.251 1.845 0 3.391.808 4.24 2.218 1.251 2.079.896 5.195-1 8.774-.245.463-.304.821-.179 1.094.305.668 1.644 1.038 3.667 1.499 1 .23 1.64.59 2.049 1.043z"/></svg>
-      <div><span>&nbsp;Profile</span></div>
-      </NavLink>
-    
-      <NavLink to="/"className="flex  hover:bg-gray-500/10 rounded border-gray-200 gap-2 ring-6  ring-inset ring-white sm:ring-0 xl:!inline-flex xl:w-auto">
-      <svg width="24" height="24"  clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498zm2.502 8.495c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25zm-3.75 0c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25zm-3.75 0c0-.69.56-1.25 1.25-1.25s1.25.56 1.25 1.25-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25z"/></svg>
-      <div><span>&nbsp;More</span></div>
-      </NavLink>
-    </nav>
+        <SideBar/>
     </div>
     
     <div>
